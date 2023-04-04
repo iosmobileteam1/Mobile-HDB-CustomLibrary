03039403d1f986db3654560e11b241fea970e021
iosmobileteam4 <iosmobileteam1@gmail.com>
2023-04-04 15:04:39 +0800

    Testing

--- Sources/Mobile@HDB CustomLibrary/CustomButton.swift
+++ Sources/Mobile@HDB CustomLibrary/CustomButton.swift
@@ -10,17 +10,17 @@ import UIKit
 
 public class CustomButton: UIButton {
 
-   public override init(frame: CGRect) {
+   override init(frame: CGRect) {
         super.init(frame: frame)
         setupButton()
     }
 
-  public  required init?(coder aDecoder: NSCoder) {
+  required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupButton()
     }
 
-   public func setupButton() {
+   func setupButton() {
 
         setTitleColor(.white, for: .normal)
 
@@ -33,7 +33,7 @@ public class CustomButton: UIButton {
 
     }
 
-   public struct btnPositive {
+   struct btnPositive {
         let font = UIScreen.main.bounds.height == 568 ? UIFont.systemFont(ofSize: UIScreen.main.bounds.height / 40) : UIFont.systemFont(ofSize: 16)
         let textColor = UIColor.white
         let backgroundColor = UIColor(red: 12/255, green: 129/255, blue: 136/255, alpha: 1)
